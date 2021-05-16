7ef126504537dd05560154f3b1645593361f9977
Einere <kjwsx23@gmail.com>
2021-05-16 17:56:01 +0900

    env: set default markup and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        header
       </header>
+        <div className={"screen lightpink"}>
+          first screen
+        </div>
+        <div className={"screen lightblue"}>
+          second screen
+        </div>
+        <div className={"screen lightgreen"}>
+          third screen
+        </div>
+        <div className={"screen lightcoral"}>
+          fourth screen
+        </div>
+        <div className={"screen lightsteelblue"}>
+          fifth screen
+        </div>
     </div>
   );
 }
